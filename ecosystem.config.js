168bf751fc744169c4c7cde51ddf658e5cdc6b65
Bruno Flegler DalCol <bruno.dalcol@el.com.br>
2018-12-05 16:11:13 -0200

    change file deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -69,8 +69,7 @@ module.exports = {
       // pre-deploy action
       //"pre-deploy-local": "echo 'This is a local executed command'",
       // post-deploy action
-      "post-deploy":
-        "npm install && pm2 reload ecosystem.config.js --env production"
+      "post-deploy": "npm install"
     }
   }
 };
