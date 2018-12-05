b4113526c8fd96e88a1f1a0ee3eaf96b1d083c24
Bruno Flegler DalCol <bruno.dalcol@el.com.br>
2018-12-05 16:23:42 -0200

    config deploy git

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -52,24 +52,11 @@ module.exports = {
         }
       ],
       port: ["9922"],
-      // SSH options with no command-line flag, see 'man ssh'
-      // can be either a single string or an array of strings
-      //ssh_options: "StrictHostKeyChecking=no",
-      // GIT remote/branch
       ref: "origin/master",
-      // GIT remote
       repo: "https://github.com/brunoflegler/kue-process.git",
-      // path in the server
       path: "~/data/app/node",
-      // Pre-setup command or path to a script on your local machine
-      //"pre-setup": "apt-get install git ; ls -la",
-      // Post-setup commands or path to a script on the host machine
-      // eg: placing configurations in the shared dir etc
-      //"post-setup": "ls -la",
-      // pre-deploy action
-      //"pre-deploy-local": "echo 'This is a local executed command'",
-      // post-deploy action
-      "post-deploy": "yarn install"
+      "pre-setup": "yarn install",
+      "post-deploy": "pm2 reload ecosystem.config.js --env production "
     }
   }
 };
