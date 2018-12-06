abb65f96c6396f834a7bc6bcbb74af6ad1d60bf2
Bruno Flegler DalCol <bruno.dalcol@el.com.br>
2018-12-06 14:07:05 -0200

    deploy atualizado

--- srv/publish.js
+++ srv/publish.js
@@ -17,7 +17,7 @@ const q = kue.createQueue({
 const jobs = () => {
   const job = q
     .create("work1", {
-      title: `Processo atualizado deploy automatico}`
+      title: `Deploy atualizado`
     })
     .attempts(3) //numeros de tentativas
     .backoff({ delay: 1 * 1000, type: "fixed" }) //atrasar o envio das falhas
